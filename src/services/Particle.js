eb8345acbb877ac07dde2abba445077f9903ded9
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-05-27 20:21:03 -0600

    update

--- src/services/Particle.js
+++ src/services/Particle.js
@@ -1,3 +1,5 @@
+import { AppState } from '../AppState'
+
 export default class Particle {
   constructor(x, y) {
     this.x = x
@@ -5,5 +7,31 @@ export default class Particle {
     this.size = Math.random() * 10 + 1
     this.weight = Math.random() * 1 + 1
     this.directionX = -10
+  }
+
+  update(canvas) {
+    if (this.y > canvas.height) {
+      this.y = Math.random() * canvas.height * 1.3
+      this.weight = Math.random() * 20 + 1
+      this.x = Math.random() * canvas.width * 1.3
+    } else if (this.y < Math.random() * this.y * 1.1 - 50) {
+      this.y = Math.random() * canvas.height * 1.3
+      this.weight = Math.random() * 20 + 1
+      this.x = Math.random() * canvas.width * 1.3
+    }
+    this.weight += 0.05
+    if (this.size > 1) {
+      this.size -= 0.001
+    }
+    this.y += this.weight
+    this.x += this.directionX
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = 'hsl(' + AppState.hue + ', 100%, 50%)'
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+    ctx.closePath()
+    ctx.fill()
   }
 }
--- src/services/ShootingStarService.js
+++ src/services/ShootingStarService.js
@@ -22,31 +22,5 @@ class ShootingStarService {
     AppState.hue++
     requestAnimationFrame(this.animate)
   }
-
-  update(canvas) {
-    if (this.y > canvas.height) {
-      this.y = Math.random() * canvas.height * 1.3
-      this.weight = Math.random() * 20 + 1
-      this.x = Math.random() * canvas.width * 1.3
-    } else if (this.y < Math.random() * this.y * 1.1 - 50) {
-      this.y = Math.random() * canvas.height * 1.3
-      this.weight = Math.random() * 20 + 1
-      this.x = Math.random() * canvas.width * 1.3
-    }
-    this.weight += 0.05
-    if (this.size > 1) {
-      this.size -= 0.001
-    }
-    this.y += this.weight
-    this.x += this.directionX
-  }
-
-  draw(ctx) {
-    ctx.fillStyle = 'hsl(' + AppState.hue + ', 100%, 50%)'
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-    ctx.closePath()
-    ctx.fill()
-  }
 }
 export const shootingStarService = new ShootingStarService()
