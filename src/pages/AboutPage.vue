5a108667ab5ca181ae6e65715a5349def269365d
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-06-12 16:22:18 -0600

    Added some margin in freelance section

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -126,7 +126,7 @@
       </div>
     </div>
     <div class="row d-flex mt-4">
-      <div class="col-12 ">
+      <div class="col-12 mb-4">
         <div class="row">
           <div class="col text-light">
             <p>
